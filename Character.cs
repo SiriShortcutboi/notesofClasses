50accef40f15c50137aee4502dd1ebb81dd68226
holden.anderson <holden.anderson@students.snow.edu>
2026-02-23 16:29:47 -0700

    broken notes, let it simmer, big concept, dont have to udnerstand now, learn more this week and after the break

---
+++ Character.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace CharacterHolder{
+    
+
+    // a class is just an object that holds related data together, you can use it as a return type
+        public class character
+        {
+        public string Namething =1;
+
+            int level = 1;
+            int health;
+            int strength;
+            int agility;
+            int gold;
+
+//Properties - PascalCase
+        public int Name {get; set;} 
+        public int Level {get; set;} 
+        public int Level {get; set;} 
+        public int Level {get; set;} 
+        public int Agility {get; set;} 
+        public int Level {get; set;} 
+        
+
+            character hero = new character() {
+            // bad thing health.name = "hero"
+            hero.Strength = 10,
+
+            hero.Agility = 5,
+            hero.health = 70,
+            
+
+            Console.WriteLine($"{hero.Name} hits {villain.Name} for {hero.strength} damage! ")
+
+            }
+            
+
+            }
+
+    
+    static void Attack(character attacker)
+        {
+            
+        }
+    }
+}
---
+++ notesofClasses/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main()
+    { 
+        
+    }
+}
